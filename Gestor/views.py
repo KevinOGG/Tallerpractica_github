13c8b639c556af39e5389bc1a78a974f57bb0b61
Danilo Astudillo <astudillodanilo@misena.edu.co>
2023-03-15 12:23:28 -0500

    agregado bootstrap y templates

--- Gestor/views.py
+++ Gestor/views.py
@@ -11,5 +11,4 @@ def inicio(request):
 def list_productos(request):
     productos=['portatil lenovo', 'Iphone galaxiA35', 'Rolex titan', 'RTX4090ti']
     contexto={'listado':productos}
-
     return render(request, 'list_productos.html', contexto)
